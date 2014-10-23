866c8e4e34c7eca78d07df0eaab043bc99436600
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2014-10-23 11:33:31 +0100

    old wip web
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

---
+++ _orig/old/index.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Purpleberry Gardens</title>
+
+<link rel="stylesheet" href="./css/bootstrap.min.css">
+<link rel="stylesheet" href="./css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="./css/styles.css">
+
+<link href="//fonts.googleapis.com/css?family=Libre+Baskerville:400,700" rel="stylesheet" type="text/css">
+
+
+<script src="./js/jquery-2.1.1.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+
+
+</head>
+
+<body>
+<div id="content">
+	<div class="logo">
+		<h1><span>Purpleberry Gardens - Award winning garden design studio in London</span></h1>
+	</div>
+	<div class="hero">
+		
+	</div>
+	<div class="menu">
+		<ul>
+			<li><a href="#" class="scroll" title="">About us</a></li>
+			<li><a href="#" class="scroll" title="">Maintenance</a></li>
+			<li><a href="#" class="scroll" title="">Hampton 2014</a></li>
+			<li><a href="#" class="scroll" title="">Who are we</a></li>
+			<li><a href="#" class="scroll" title="">Portfolio</a></li>
+			<li><a href="#" class="scroll" title="">Contacts</a></li>
+		</ul>
+	</div>
+	<div class="section about">
+		<div class="header">
+			<h2>About Us</h2>
+		</div>
+		<div class="content">
+			<p><strong>Katerina Rafaj Garden Design</strong>. specialises in the creation of beautiful gardens for private individuals and commercial organisations all over the South of England. Our varied projects covering roofs, terraces, community gardens and back gardens, range from tiny jewel box terraces to expansive open plan lawns and spacess. We provide sound, innovative solutions to the horticultural challenges posed by city gardening and bring the creativity you are looking for to liven up your outside living space wherever it may be.</p>
+		</div>
+	</div>
+	<div class="section maintenance">
+		<div class="header">
+			<h2>Maintenance</h2>
+		</div>
+		<div class="content">
+			<p>Welcome to Purpleberry Gardens, based in London and Brighton.</p>
+ 
+			<p>Having enjoyed successes at the RHS Flower Show and with experience of designing both private gardens and public spaces, Purpleberry Gardens is the creation of Katerina Rafaj and Daniela Krasnanova.</p>
+			 
+			<p>Graduates of London College of Garden Design and Greenwich University respectively, Katerina and Daniela’s shared vision and passion is to create sustainable gardens that also meet the complex wishes and needs of their clients. The pair are also committed to educating young people in all aspects of gardening and horticulture, and they’re currently in the process of establishing an exciting charity program to this effect.</p>
+			 
+			<p>Attention to detail lies at the heart of all Katerina and Daniela do, and the service you’ll receive with Purpleberry Gardens includes not only preliminary planning and concept visits, but also after-care checks and careful maintenance scheduling. Daniela and Katerina only work with recognised and reputable contractors, those who they trust to carry out works with absolute professionalism and attention to detail.</p>
+		</div>
+	</div>
+	<div class="section hampton">
+		<div class="header">
+			<h2>Hampton Court 2014</h2>
+		</div>
+		<div class="content">
+			<p>GLUTTONY, one of the deadly sins we so very often and very willingly forget about. Our garden, named <strong>E123</strong> after one of the worst food additives, is a way to represent and describe this sin in an abstract, artistic and playful manner. In this garden we're highlighting the great amount of food being consumed and wasted every day in the UK and other western worlds through the over-sized sculptures, not mistakenly designed to remind people of the iconic food brands of our time, which should not only attract the eye, but also point out the enormous and unnecessary levels of food consumption whilst millions of people, including children starve.
+		</div>
+	</div>
+	<div class="section process">
+		<div class="header">
+			<h2>Process</h2>
+		</div>
+		<div class="content">
+			<p>Garden design is the art of turning a plain space into a beautiful garden. It’s about seeing, and then transforming, potential. As garden designers we bring together our skills in art and design as well as our horticultural knowledge and training to deliver a truly comprehensive service to our clients. We dig and plant, source all materials, manage hard landscaping elements such as paths, paving and decking, and we organise the installation of all additional structures like lighting, furniture and water features.</p>
+		</div>
+	</div>
+	<div class="people">
+		<div class="header">
+			<h2>Who are we</h2>
+		</div>
+		<div class="content row">
+			<div class="person col-md-6">
+				<img src="./img/maintenance.jpg" alt="" />
+				<h2>Katerina Rafaj</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper pretium quam quis pharetra. Cras pretium dui mi, nec tincidunt urna consectetur ac. Nam odio felis, rutrum quis rhoncus non, vestibulum eget risus.</p>
+			</div>
+			<div class="person col-md-6">
+				<img src="./img/maintenance.jpg" alt="" />
+				<h2>Tai Ahranov</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper pretium quam quis pharetra. Cras pretium dui mi, nec tincidunt urna consectetur ac. Nam odio felis, rutrum quis rhoncus non, vestibulum eget risus.</p>
+			</div>
+		</div>
+	</div>
+	<div class="portfolio">
+		<div class="header">
+			<h2>Portfolio</h2>
+		</div>
+		<div class="content">
+			<div class="work">
+				<img src="" align="" />
+			</div>
+			<div class="work">
+				<img src="" align="" />
+			</div>
+			<div class="work">
+				<img src="" align="" />
+			</div>
+			<div class="work">
+				<img src="" align="" />
+			</div>
+			<div class="work">
+				<img src="" align="" />
+			</div>
+		</div>
+	</div>
+	<div class="contact">
+		<div class="header">
+			<h2>Contact</h2>
+		</div>
+		<div class="content">
+			<div class="place">
+				<h3>London</h3>
+				<p class="map"><img src="" align="London map" /></p>
+				<p class="email"><a href="mailto:katerina.rafaj@gmail.com" title="Email Katerina Rafaj"><i class="fa fa-mail"></i>Katerina Rafaj</a></p>
+				<p class="address">
+					71 Avondale Avenue<br />
+					NE4 8NB<br />
+					Barnet, London<br />
+				</p>
+				<p class="phone">Tel.: +44 (0) 7588 11 21 31</p>
+			</div>
+			<div class="place">
+				<h3>Brighton</h3>
+				<p class="map"><img src="" align="Brighton map" /></p>
+				<p class="email"><a href="mailto:krasnanovadaniela@yahoo.com" title="Email Daniela Krasnanova"><i class="fa fa-mail"></i>Daniela Krasnanova</a></p>
+				<p class="address">
+					71 Avondale Avenue<br />
+					NE4 8NB<br />
+					Barnet, London<br />
+				</p>
+				<p class="phone">Tel.: +44 (0) 783 798 24 52</p>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<a href="#" title=""><i class="fa fa-twitter"></i>Twitter</a>
+	</div>
+</div>
+<script src="./js/retina.min.js"></script>
+</body>
+</html>
